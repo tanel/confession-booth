372594a9d20c42895e34c9140a1631e7e16e45a1
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-05-19 23:33:51 +0300

    Coin reader

---
+++ Arduino/coin-reader/coin-reader.ino
@@ -0,0 +1,31 @@
+
+const int coinInt = 1;
+
+volatile float coinsValue = 0.00;
+
+int coinsChange = 0;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(coinInt, coinInserted, RISING);
+  Serial.println("setup done");
+}
+
+void coinInserted() {
+  coinsValue = coinsValue + 2;
+  coinsChange = 1;
+}
+
+void loop() {
+  // check if coin inserted
+  if (coinsChange == 1) {
+    // unflag that a coin has been inserted
+    coinsChange = 0;
+
+    Serial.print("Credit: $");
+    Serial.println(coinsValue);
+  }
+
+  delay(1000);
+}
+
