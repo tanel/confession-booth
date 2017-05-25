5935ace7b5955e1ee51dadb2d9a13c360afe82f9
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-05-26 01:48:02 +0300

    Change video

--- of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.cpp
+++ of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    ofSetWindowShape(kMovieSize, kMovieSize);
+    ofSetWindowShape(kMovieSize-120, kMovieSize);
     
 	ofBackground(255,255,255);
 	ofSetVerticalSync(true);
@@ -11,7 +11,7 @@ void ofApp::setup(){
 	// Uncomment this to show movies with alpha channels
 	movie.setPixelFormat(OF_PIXELS_RGBA);
 
-	movie.load("movies/videoplayback.mp4");
+	movie.load("movies/Bardo_Btson.mp4");
 	movie.setLoopState(OF_LOOP_NORMAL);
     
     ofSetLogLevel(OF_LOG_VERBOSE);
@@ -19,18 +19,21 @@ void ofApp::setup(){
     serial.listDevices();
     vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
     
-    // this should be set to whatever com port your serial device is connected to.
-    // (ie, COM4 on a pc, /dev/tty.... on linux, /dev/tty... on a mac)
-    // arduino users check in arduino app....
     int baud = 9600;
-    serial.setup(0, baud); //open the first device
-    //serial.setup("COM4", baud); // windows example
-    //serial.setup("/dev/tty.usbserial-A4001JEC", baud); // mac osx example
-    //serial.setup("/dev/ttyUSB0", baud); //linux example
+    serial.setup(0, baud);
     
     nBytesRead = 0;
-    readTime = 0;
+    startTime = 0;
     memset(bytesReadString, 0, kBufSize);
+}
+
+void ofApp::pause(const bool doPause) {
+    paused = doPause;
+    movie.setPaused(paused);
+    
+    if (!paused) {
+        startTime = ofGetElapsedTimef();
+    }
 }
 
 //--------------------------------------------------------------
@@ -39,8 +42,7 @@ void ofApp::update(){
     
     if(movie.getIsMovieDone()){
         movie.firstFrame();
-        paused = true;
-        movie.setPaused(paused);
+        pause(true);
     }
     
     if(serial.available() > 0) {
@@ -58,43 +60,72 @@ void ofApp::update(){
         
         if (nBytesRead > 0) {
             if (movie.isPaused()) {
-                paused = false;
-                movie.play();
+                pause(false);
             }
         }
         
         memcpy(bytesReadString, bytesReturned, kBufSize);
-        
-        readTime = ofGetElapsedTimef();
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 	ofSetHexColor(0xFFFFFF);
+    
+    int videoSize = kMovieSize/1.2;
 
-    movie.draw(0,0,kMovieSize,kMovieSize);
+    movie.draw(0, 0, videoSize, videoSize);
     
     ofSetHexColor(0x000000);
+    
+    int textStart = kMovieSize - 80;
 	
-    ofDrawBitmapString("p=(un)pause, 0=rewind",20,320);
-    ofDrawBitmapString("frame: " + ofToString(movie.getCurrentFrame()) + "/"+ofToString(movie.getTotalNumFrames()),20,340);
-    ofDrawBitmapString("duration: " + ofToString(movie.getPosition()*movie.getDuration(),2) + "/"+ofToString(movie.getDuration(),2),20,360);
-    ofDrawBitmapString("last coin time " + ofToString(readTime, 3), 20, 380);
+    ofDrawBitmapString("<space>=(un)pause, 0-9=jump",
+                       20, textStart+0);
+    ofDrawBitmapString("frame: " + ofToString(movie.getCurrentFrame()) + "/"+ofToString(movie.getTotalNumFrames()),
+                       20, textStart+20);
+    ofDrawBitmapString("duration: " + ofToString(movie.getPosition()*movie.getDuration(),2) + "/"+ofToString(movie.getDuration(),2),
+                       20, textStart+40);
+    ofDrawBitmapString("last coin time " + ofToString(startTime, 3),
+                       20, textStart+60);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed  (int key){
     switch(key){
-        case 'p':
-            paused = !paused;
-            movie.setPaused(paused);
-        break;
+        case ' ':
+            pause(!paused);
+            break;
         case '0':
-            movie.firstFrame();
-            paused = true;
-            movie.setPaused(paused);
-        break;
+            movie.setFrame(movie.getTotalNumFrames() * 0.0);
+            break;
+        case '1':
+            movie.setFrame(movie.getTotalNumFrames() * 0.1);
+            break;
+        case '2':
+            movie.setFrame(movie.getTotalNumFrames() * 0.2);
+            break;
+        case '3':
+            movie.setFrame(movie.getTotalNumFrames() * 0.2);
+            break;
+        case '4':
+            movie.setFrame(movie.getTotalNumFrames() * 0.4);
+            break;
+        case '5':
+            movie.setFrame(movie.getTotalNumFrames() * 0.5);
+            break;
+        case '6':
+            movie.setFrame(movie.getTotalNumFrames() * 0.6);
+            break;
+        case '7':
+            movie.setFrame(movie.getTotalNumFrames() * 0.7);
+            break;
+        case '8':
+            movie.setFrame(movie.getTotalNumFrames() * 0.8);
+            break;
+        case '9':
+            movie.setFrame(movie.getTotalNumFrames() * 0.9);
+            break;
     }
 }
 
--- of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.h
+++ of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.h
@@ -14,15 +14,16 @@ class ofApp : public ofBaseApp{
 		void draw();
 		
 		void keyPressed(int key);
+        void pause(const bool doPause);
 
 		ofVideoPlayer 		movie;
 		bool                paused;
+        float		startTime;
     
         bool		bSendSerialMessage;			// a flag for sending serial
         char		bytesRead[kBufSize];				// data from serial, we will be trying to read 3
         char		bytesReadString[kBufSize];			// a string needs a null terminator, so we need 3 + 1 bytes
         int			nBytesRead;					// how much did we read?
-        float		readTime;					// when did we last read?
     
         ofSerial	serial;
     
