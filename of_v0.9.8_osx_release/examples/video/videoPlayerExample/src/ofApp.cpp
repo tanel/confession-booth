e7e6f7e852bf6b878985ca3955a79a0525951e99
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-05-26 20:05:57 +0300

    Add audio input

--- of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.cpp
+++ of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.cpp
@@ -8,14 +8,16 @@ void ofApp::setup(){
 	ofSetVerticalSync(true);
 	paused = true;
 
-	// Uncomment this to show movies with alpha channels
+	// Show movies with alpha channels
 	movie.setPixelFormat(OF_PIXELS_RGBA);
 
+    // Load video
 	movie.load("movies/Bardo_Btson.mp4");
 	movie.setLoopState(OF_LOOP_NORMAL);
     
     ofSetLogLevel(OF_LOG_VERBOSE);
-    
+
+    // Initialize serial input
     serial.listDevices();
     vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
     
@@ -23,8 +25,28 @@ void ofApp::setup(){
     serial.setup(0, baud);
     
     nBytesRead = 0;
-    startTime = 0;
     memset(bytesReadString, 0, kBufSize);
+    
+    // Initialize coin input
+    startTime = 0;
+    
+    // Initialize audio input
+    //if you want to set a different device id
+    //soundStream.setDeviceID(0); //bear in mind the device id corresponds to all audio devices, including  input-only and output-only devices.
+    soundStream.printDeviceList();
+
+    int bufferSize = 256;
+    
+    left.assign(bufferSize, 0.0);
+    right.assign(bufferSize, 0.0);
+    volHistory.assign(400, 0.0);
+    
+    bufferCounter	= 0;
+    drawCounter		= 0;
+    smoothedVol     = 0.0;
+    scaledVol		= 0.0;
+    
+    soundStream.setup(this, 0, 2, 44100, bufferSize, 4);
 }
 
 void ofApp::pause(const bool doPause) {
@@ -148,3 +170,31 @@ void ofApp::keyPressed  (int key){
     }
 }
 
+//--------------------------------------------------------------
+void ofApp::audioIn(float * input, int bufferSize, int nChannels){
+    float curVol = 0.0;
+    
+    // samples are "interleaved"
+    int numCounted = 0;
+    
+    //lets go through each sample and calculate the root mean square which is a rough way to calculate volume
+    for (int i = 0; i < bufferSize; i++){
+        left[i]		= input[i*2]*0.5;
+        right[i]	= input[i*2+1]*0.5;
+        
+        curVol += left[i] * left[i];
+        curVol += right[i] * right[i];
+        numCounted+=2;
+    }
+    
+    //this is how we get the mean of rms :)
+    curVol /= (float)numCounted;
+    
+    // this is how we get the root of rms :)
+    curVol = sqrt( curVol );
+    
+    smoothedVol *= 0.93;
+    smoothedVol += 0.07 * curVol;
+    
+    bufferCounter++;
+}
--- of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.h
+++ of_v0.9.8_osx_release/examples/video/videoPlayerExample/src/ofApp.h
@@ -8,26 +8,40 @@
 class ofApp : public ofBaseApp{
 
 	public:
-
 		void setup();
 		void update();
 		void draw();
-    
-		void keyPressed(int key);
+
+        // User input
+        void keyPressed(int key);
         void pause(const bool doPause);
-    
         void windowResized(int width, int height);
 
+        // Video player
 		ofVideoPlayer 		movie;
 		bool                paused;
+
+        // Coin input
         float		startTime;
-    
+
+        // Serial
         bool		bSendSerialMessage;			// a flag for sending serial
         char		bytesRead[kBufSize];				// data from serial, we will be trying to read 3
         char		bytesReadString[kBufSize];			// a string needs a null terminator, so we need 3 + 1 bytes
         int			nBytesRead;					// how much did we read?
-    
         ofSerial	serial;
     
+        // Audio input
+        void audioIn(float * input, int bufferSize, int nChannels);
+    
+        vector <float> left;
+        vector <float> right;
+        vector <float> volHistory;
+        int 	bufferCounter;
+        int 	drawCounter;
+        float smoothedVol;
+        float scaledVol;
+    
+        ofSoundStream soundStream;
 };
 
